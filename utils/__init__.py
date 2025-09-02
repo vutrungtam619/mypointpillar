ab4f345b4382ba2c89e5680ccd82d783223ffb4a
vutrungtam619 <vutrungtam619@gmail.com>
2025-09-02 20:45:57 +0700

    Add

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,9 @@
 from .io import read_calib, read_label, read_pickle, read_points, write_label, write_pickle, write_points
-from .anchor import Anchors, anchor_target, anchors2bboxes
 from .process import project_point_to_camera, bbox_camera2lidar, bbox3d2bevcorners, box_collision_test, \
     remove_pts_in_bboxes, limit_period, bbox3d2corners, points_lidar2image, \
     keep_bbox_from_image_range, keep_bbox_from_lidar_range, \
     points_camera2lidar, setup_seed, remove_outside_points, points_in_bboxes_v2, \
     get_points_num_in_bbox, iou2d_nearest, iou2d, iou3d_camera, iou_bev, \
     bbox3d2corners_camera, points_camera2image
+from .anchor import Anchors, anchor_target, anchors2bboxes
 from .loss import Loss
